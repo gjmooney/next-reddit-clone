2d5c531cd4d4d8411cdbadc6388d341e103db5bc
Greg <gjmooney@asu.edu>
2023-06-26 12:01:56 +0200

    Create Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import { Icons } from "./Icons";
+
+const Navbar = () => {
+  return (
+    <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc0199 z-[10] py-2">
+      <div className="container mx-w-7xl h-full mx-auto flex items-center justify-between gap-2">
+        {/* logo */}
+        <Link href="/" className="flex gap-2 items-center">
+          <Icons.logo className="h-8 w-8 sm:h-6 sm:w-6" />
+          <p className="hidden text-black-100 text-sm font-medium md:block">
+            Breadit
+          </p>
+        </Link>
+
+        {/* Search bar */}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
