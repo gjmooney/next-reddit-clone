4ce902fd8d83fc41c8df1ef2412027633726b271
Greg <gjmooney@asu.edu>
2023-06-26 17:06:07 +0200

    Start adding Dropdown to Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,12 @@
 import Link from "next/link";
 import { Icons } from "./Icons";
 import { buttonVariants } from "./ui/Button";
+import UserAccountNav from "./UserAccountNav";
+import { getAuthSession } from "@/lib/auth";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await getAuthSession();
+
   return (
     <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc0199 z-[10] py-2">
       <div className="container mx-w-7xl h-full mx-auto flex items-center justify-between gap-2">
@@ -17,10 +21,13 @@ const Navbar = () => {
         {/* Search bar */}
 
         {/* Login */}
-        {/* buttonVariants makes link look like a button */}
-        <Link href="/sign-in" className={buttonVariants()}>
-          Sign In
-        </Link>
+        {session?.user ? (
+          <UserAccountNav user={session.user} />
+        ) : (
+          <Link href="/sign-in" className={buttonVariants()}>
+            Sign In
+          </Link>
+        )}
       </div>
     </div>
   );
