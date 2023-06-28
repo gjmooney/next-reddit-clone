23f564146e3481e6956ca3eee9c9c6a896dc495a
Greg <gjmooney@asu.edu>
2023-06-28 17:33:22 +0200

    Add create post endpoint

---
+++ src/app/api/subreddit/post/create/route.ts
@@ -0,0 +1,53 @@
+import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { PostValidator } from "@/lib/validators/post";
+import { SubredditSubscriptionValidator } from "@/lib/validators/subreddit";
+import { z } from "zod";
+
+export async function POST(req: Request) {
+  try {
+    const session = await getAuthSession();
+
+    // Make sure user is logged in
+    if (!session?.user) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    const body = await req.json();
+
+    const { title, content, subredditId } = PostValidator.parse(body);
+
+    // check is already subscribed
+    const subscriptionExists = await db.subscription.findFirst({
+      where: {
+        subredditId,
+        userId: session.user.id,
+      },
+    });
+
+    if (!subscriptionExists) {
+      return new Response(
+        "You must be subscribed to a community to create a post there",
+        { status: 400 }
+      );
+    }
+
+    await db.post.create({
+      data: {
+        title,
+        content,
+        authorId: session.user.id,
+        subredditId,
+      },
+    });
+
+    return new Response("Post created");
+  } catch (error) {
+    // Type parsing error
+    if (error instanceof z.ZodError) {
+      return new Response("Invalid request data passed", { status: 422 });
+    }
+
+    return new Response("Could not create new post", { status: 500 });
+  }
+}
