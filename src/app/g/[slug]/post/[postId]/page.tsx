60f5e07fe444e6226031eb9886dad05720c8037a
Greg <gjmooney@asu.edu>
2023-06-30 13:01:24 +0200

    Add post details

--- src/app/g/[slug]/post/[postId]/page.tsx
+++ src/app/g/[slug]/post/[postId]/page.tsx
@@ -1,12 +1,14 @@
+import EditorOutput from "@/components/EditorOutput";
 import PostVoteServer from "@/components/post-vote/PostVoteServer";
 import { buttonVariants } from "@/components/ui/Button";
 import { db } from "@/lib/db";
 import { redis } from "@/lib/redis";
+import { formatTimeToNow } from "@/lib/utils";
 import { CachedPost } from "@/types/redis";
 import { Post, User, Vote } from "@prisma/client";
 import { ArrowBigDown, ArrowBigUp, Loader2 } from "lucide-react";
 import { notFound } from "next/navigation";
-import { FC, Suspense } from "react";
+import { Suspense } from "react";
 
 interface PageProps {
   params: {
@@ -45,6 +47,7 @@ const page = async ({ params }: PageProps) => {
   return (
     <div>
       <div className="h-full flex flex-col sm:flex-row items-center sm:items-start justify-between">
+        {/** Vote stuff */}
         <Suspense fallback={<PostVoteShell />}>
           {/* @ts-expect-error server component */}
           <PostVoteServer
@@ -61,6 +64,19 @@ const page = async ({ params }: PageProps) => {
             }}
           />
         </Suspense>
+
+        {/** post details */}
+        <div className="sm:w-0 w-full flex-1 bg-white p-4 rounded-sm">
+          <p className="max-h-[40] mt-1 truncate text-xs text-gray-500">
+            Posted by u/{post?.author.username ?? cachedPost.authorUsername}{" "}
+            {formatTimeToNow(new Date(post?.createdAt ?? cachedPost.createdAt))}
+          </p>
+          <h1 className="text-xl font-semibold py-2 leading-6 text-gray-900">
+            {post?.title ?? cachedPost.title}
+          </h1>
+
+          <EditorOutput content={post?.content ?? cachedPost.content} />
+        </div>
       </div>
     </div>
   );
